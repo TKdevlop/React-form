0e7e555d048aa2f6c0d7484c4c7540acf7473a5b
TUSHAR KHARBANDA <tusharkharbanda13@gmail.com>
2018-11-06 15:52:37 +0530

    Updates

---
+++ precache-manifest.4c8082df085e489f6b991c644350e4f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fb95a0a024ec0187ecc9",
+    "url": "/static/js/main.fb95a0a0.chunk.js"
+  },
+  {
+    "revision": "9e41b7c90c46e3e7b489",
+    "url": "/static/js/1.9e41b7c9.chunk.js"
+  },
+  {
+    "revision": "fb95a0a024ec0187ecc9",
+    "url": "/static/css/main.81029aca.chunk.css"
+  },
+  {
+    "revision": "b9dd2923ecb13da44ef6379cbdeb5812",
+    "url": "/index.html"
+  }
+];
